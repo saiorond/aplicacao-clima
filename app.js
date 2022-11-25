6f3d75cd6740d9caddea056f6e7721b1a09d1c53
Débora <d.saioron@hotmail.com>
2022-11-24 21:19:41 -0300

    iniciando js

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-// 90204aec3b7c0d3589459a17ea2beb80
+// 
+const apiKey = "90204aec3b7c0d3589459a17ea2beb80";
+const apiCountry = "https://countryflagsapi.com/png/";
+
+const cityInput = document.querySelector("#city-input");
+const searchBtn = document.querySelector('#search');
+
