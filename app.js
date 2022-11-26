911145aa6cbe6481e075efb995d797e316bbcdfd
Débora <d.saioron@hotmail.com>
2022-11-25 21:14:50 -0300

    js

--- app.js
+++ app.js
@@ -5,3 +5,14 @@ const apiCountry = "https://countryflagsapi.com/png/";
 const cityInput = document.querySelector("#city-input");
 const searchBtn = document.querySelector('#search');
 
+const showWeatherData = (city) => {
+    console.log(city)
+ }
+
+searchBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const city = cityInput.value;
+
+    console.log(showWeatherData);
+})
