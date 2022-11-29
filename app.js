fab69afb4b23537b905f96a0905ecdb2981f52f2
Débora <d.saioron@hotmail.com>
2022-11-29 20:32:09 -0300

    alterando as informações para informações da api

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // 
 const apiKey = "90204aec3b7c0d3589459a17ea2beb80";
-const apiCountry = "https://countryflagsapi.com/png/";
+const apiCountryURL = "https://countryflagsapi.com/png/";
 
 const cityInput = document.querySelector("#city-input");
 const searchBtn = document.querySelector('#search');
@@ -8,9 +8,9 @@ const searchBtn = document.querySelector('#search');
 const cityElement = document.querySelector("#city");
 const tempElement = document.querySelector("#temperature span");
 const descElement = document.querySelector("#description");
-const watherIconElement = document.querySelector("#weather-icon");
+const weatherIconElement = document.querySelector("#weather-icon");
 const countryElement = document.querySelector("#country");
-const umidityElement = document.querySelector("#umidity");
+const humidityElement = document.querySelector("#umidity");
 const windElement = document.querySelector("#wind span");
 
 
@@ -20,13 +20,24 @@ const getWeatherData = async (city) => {
     const res = await fetch(apiWeatherURL);
     const data = await res.json();
 
-    console.log(data)
+    return data;
 }
 
 
-const showWeatherData = (city) => {
-    getWeatherData(city)
- }
+const showWeatherData = async (city) => {
+    const data = await getWeatherData(city)
+ 
+    cityElement.innerText = data.name;
+    tempElement.innerText = parseInt(data.main.temp);
+    descElement.innerText = data.weather[0].description;
+    weatherIconElement.setAttribute(
+        "src",
+        `https://openweathermap.org/img/wn/${ data.weather[0].icon}.png`
+    );
+    countryElement.setAttribute("src", apiCountryURL + data.sys.country);
+    humidityElement.innerText = `${data.main.humidity}%`;
+    windElement.innerText = `${data.wind.speed}km/h`;
+}
 
 searchBtn.addEventListener("click", (e) => {
     e.preventDefault();
